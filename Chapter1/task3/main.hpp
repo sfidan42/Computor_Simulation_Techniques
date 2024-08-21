7b933a8cb265cac0e18b73667d7d2c85f8dade03
sfidan42 <sadettinfidan00@gmail.com>
2024-08-21 11:36:55 +0300

    update

--- Chapter1/task3/main.hpp
+++ Chapter1/task3/main.hpp
@@ -2,6 +2,7 @@
 # include <iostream>
 
 # define NaN (unsigned int)-1
+# define MAX (unsigned int)-1
 
 enum e_server_status { idle, busy, down, blocked };
 
@@ -19,7 +20,16 @@ public:
 	unsigned int	get_dep_time(void) const { return (_dep_time); }
 	unsigned int	get_brk_down(void) const { return (_brk_down); }
 	unsigned int	get_operational(void) const { return (_operational); }
+	void			inc_n_cust(void) { _n_cust++; }
+	void			inc_dep_time(void) { _dep_time++; }
+	void			inc_brk_down(void) { _brk_down++; }
+	void			inc_operational(void) { _operational++; }
+	void			dec_n_cust(void) { _n_cust--; }
+	void			dec_dep_time(void) { _dep_time--; }
+	void			dec_brk_down(void) { _brk_down--; }
+	void			dec_operational(void) { _operational--; }
 	e_server_status	get_status(void) const { return (_status); }
+	void			set_status(e_server_status status){ _status = status; }
 };
 
 std::ostream	&operator<<(std::ostream &os, const stage *s);
@@ -31,6 +41,7 @@ private:
 	unsigned int	_arr_time;
 	stage			*_stages[2];
 private:
+	void	update_master_clock(void);
 	bool	schedule(void);
 	void	display(void);
 public:
--- Chapter1/task3/src/simulator.cpp
+++ Chapter1/task3/src/simulator.cpp
@@ -14,9 +14,26 @@ simulator::~simulator(void)
 	delete _stages[1];
 }
 
+void	simulator::update_master_clock(void)
+{
+	unsigned int	min_clk = MAX;
+
+	if (_arr_time < min_clk)
+	{
+		min_clk = _arr_time;
+	}
+	_master_clock = min_clk;
+}
+
 bool	simulator::schedule(void)
 {
-	return (false);
+	update_master_clock();
+	if (_arr_time == _master_clock)
+	{
+		_stages[0]->inc_n_cust();
+		_arr_time += 40;
+	}
+	return (true);
 }
 
 void	simulator::display(void)
@@ -34,10 +51,16 @@ void	simulator::display(void)
 
 void	simulator::run(void)
 {
+	unsigned int	max_rows = 30;
+
 	std::cout << "\t------------------stage 1--------------------"
 		<< "\t---------------stage 2---------------\n"
 		<< "MC\tAT\t#Cust\tDT1\tBR1\tOP1\tStat1\t#Cust\tDT2\tBR2\tOP2\tStat2\n";
 	display();
-	while (schedule())
+	while (schedule() && max_rows--)
+	{
 		display();
+		if (_master_clock >= 20)
+			break ;
+	}
 }
